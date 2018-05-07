1a2aa613d0101274000f41bd8127c27e82f82e16
Luis Alberto Acevedo Rojas <laacevedodesarrollodotnet@hotmail.com>
2018-05-07 01:23:24 -0500

    Integración de ensamblado de bussinnes.

--- Example.GitHub/Example.GitHub.UI/Program.cs
+++ Example.GitHub/Example.GitHub.UI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Example.GitHub.Bussiness;
 
 namespace Example.GitHub.UI
 {
@@ -11,6 +12,13 @@ namespace Example.GitHub.UI
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo desde Git-hub");
+
+            var employee = new Employee();
+            employee.Name = "Luis";
+            employee.LastName = "Acevedo";
+            employee.Age = "28 age";
+            Console.WriteLine("Employe> {0} {1} {2}", employee.Name, employee.LastName, employee.Age);
+            Console.ReadLine();
         }
     }
 }
